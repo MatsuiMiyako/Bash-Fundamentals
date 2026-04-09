8bbb3fe776acae0cf856aea567eb3ae9fe85b7fb
MatsuiMiyako <rowlet997@gmail.com>
2026-04-09 10:13:20 -0500

    Added descrition of variables.

--- variables.sh
+++ variables.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+# What are variables?
+# Variables are names given to the computer that can store values or data.
+# Basically, think of it as storing 'object(s)' in a 'box'. 
+
+# Why use variables?
+# You may want to store some data for ease of access or later usage. 
+# You can also manipulate or change the value of the variable to make programs flexible.
+
+# How do variables work?
+# In Bash, variables are untyped, which means they can hold any type of value.
+# Other languages may need you to declare what type of 'object(s)' can go into the 'box.'
+
+
+
+
+
+
+
+
