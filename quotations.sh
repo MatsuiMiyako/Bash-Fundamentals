c80bf92641fa85f97466cc2c32481f93b1b075e9
MatsuiMiyako <rowlet997@gmail.com>
2026-04-10 10:55:18 -0500

    Added common error examples and finished off the documentation

--- quotations.sh
+++ quotations.sh
@@ -36,3 +36,14 @@ BOBBIN="Gick Drayson"
 echo 'Ratman said to Bobbin: "I am $RATMAN, the greatest hewo in the world!"'  
 # This will literally output: Ratman said to Bobbin: "I am $RATMAN, the greatest hewo in the world!"
 
+# What not to do?
+# If you want to use variables or have special characters interpreted, don't use single quotes.
+# If you want to treat everything literally, don't use double quotes.
+
+# Example of what to avoid:
+RATMAN="Wuce Brayne"
+echo 'Hello, $RATMAN!'  # This will not output the value of RATMAN
+
+# Make sure to use the appropriate type of quotation based on your needs.
+
+# ---------------------------------------------------------------------------------------------------------------------
