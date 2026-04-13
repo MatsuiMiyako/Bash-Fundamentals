bb6345f21076e3ca36cabbc4d65378bd15016e71
MatsuiMiyako <rowlet997@gmail.com>
2026-04-13 10:25:58 -0500

    Added printf descritions

---
+++ printf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash printf
+
+# What is printf?
+# The command `printf` is used to format output with more control than the options you get from the `echo` command
+# Basically, it is a fancier and more nuanced version of `echo`
+# The Bash `printf` command is modeled on the `printf` function from the C language for those who are familiar with C (not me)
+
+# Why use printf?
+# You may already be familiar with the command `echo` and you may be wondering why `printf` even exists
+# Another advantage of printf is its portability;
+# printf stays the same across different shells (fsh, sh, bash)
+
+# How does printf work?
+# Literal text is printed as is
+# Format specifiers start with '%'
+# Syntax: printf FORMAT [ARGUEMENTS...]
+
+
