fa16e8b046a792ad204e29b2b8dcbe9dc2c8191f
MatsuiMiyako <rowlet997@gmail.com>
2026-04-14 10:11:44 -0500

    Added examples 3 and 4

--- printf.sh
+++ printf.sh
@@ -18,11 +18,11 @@
 # Literal text is printed as is
 # Format specifiers start with '%'
 # Syntax: printf "formatted-string" [arguements...]
-# printf does not automatically newline
+# printf does not automatically newline; use \n to newline
 
 # Common Format Specifiers:
 # %s - strings
-# %d - integer (decimal)
+# %d - integer
 # %f - floating-point
 # %x - hexadecimal (lowercase)
 # %o - octal
@@ -31,11 +31,22 @@
 # Example 1): 
 printf "Greetings, neighbor! The name's %s. But you can just call me %s.\n" "Crazy Dave" "Crazy Dave"
 # Output: Greetings, neighbor! The name's Crazy Dave. But you can just call me Crazy Dave.
-# The arguments "Crazy Dave" and "Crazy Dave" are substituted into the format string where the %s specifiers are located, in order
+# The arguments "Crazy Dave" and "Crazy Dave" are substituted into the format string 
+# where the %s specifiers are located, in order
 
 # Example 2):
 number=67
 printf "The number is %d.\n" "$number"
 # Output: The number is 67.
 
+# Example 3):
+printf "%.2f\n" 1.337
+# Output: 1.34
+# I rounded the number to two decimal points
 
+# Example 4):
+printf "%x\n" 61669
+# Output: f0e5
+# the %o works in a similar fashion
+printf "%o\n" 266305
+# Output: 1010101
