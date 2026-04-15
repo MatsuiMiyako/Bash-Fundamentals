3bbb90a93a5f20fde81048403ea2f493679eaa44
MatsuiMiyako <rowlet997@gmail.com>
2026-04-15 10:37:19 -0500

    Added description for bash brackets

--- brackets.sh
+++ brackets.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash brackets
+
+# What are brackets?
+# Believe it or not, there are differences between brackets: `[]`, `[[]]`; parentheses: `()`, `(())`;
+# and curly braces: `{}`.
+# Each symbol or symbols have a different job in bash and misuse can lead to unexpected behaviors in your scripts.
+
+# Why use brackets?
+# Brackets in bash are used for conditionals, parameters, arithmetic, and command grouping.
+# Basically, mastering them will be helpful.
+
+# How do brackets work?
+# 
