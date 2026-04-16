124bc5f24f9e4f1731c69de50b11c252389685cc
MatsuiMiyako <rowlet997@gmail.com>
2026-04-16 10:41:12 -0500

    Added example 2

--- brackets.sh
+++ brackets.sh
@@ -19,9 +19,15 @@
 # Parentheses in Bash are used to create a subshell, a "child" that runs independantly from the current shell.
 
 # Example 1):
-( cd /etc; cat hostname )
+(cd /etc; cat hostname)
 # Output: my-computer-name
 # The parentheses create a subshell where it changes your directory to `/etc`, which then runs the second command.
 # The second command is also in the subshell, and is executed after the first command because of the semicolon (;),
-# which allows for multiple commands to be executed in order
+# which allows for multiple commands to be executed in order.
+# All of that is run in a subshell so your present working directory in the parent shell doesn't change.
 
+# Example 2):
+
+(age=17; grade=11; echo "My age is $age") # Output: My age is 17
+echo "My grade is $grade" # Output: My grade is
+# The variable `grade` is defined only in the subshell; the parent shell does not have the variable `grade`.
