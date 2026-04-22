bda6f762b1cd57466e9e100fec0fd5206828601c
MatsuiMiyako <rowlet997@gmail.com>
2026-04-21 20:16:23 -0500

    Added conditionals description

--- conditionals.sh
+++ conditionals.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Conditionals
+
+# What are conditionals?
+# Conditionals in bash are used to perform different actions based on different conditions, 
+# hence the word conditionals.
+# Basically, they allow you to control the flow of your script based on certain criteria.
+
+# Why use conditionals?
+# Conditionals are important for making decisions in your scripts.
+# They allow you to execute different blocks of code based on the outcome of a condition,
+# which can be useful for tasks like error handling, user input validation, etc.
+
+# How do conditionals work?
+# The most common conditional statements in bash are `if`, `elif`, and `else`
+
+
