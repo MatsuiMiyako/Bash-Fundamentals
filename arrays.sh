49d956bea0b88c885742caef4d29610a3be7d4ee
MatsuiMiyako <rowlet997@gmail.com>
2026-04-23 18:59:25 -0500

    Added description to bash arrays

--- arrays.sh
+++ arrays.sh
@@ -5,4 +5,10 @@
 # Bash Arrays
 
 # What are arrays?
+# Arrays in bash is a single variable that can hold multiple values.
+# These values can be accessed using an index, which starts at 0.
+# Some languages start their array index at 1, but bash starts at 0, so keep that in mind.
+
+
+
 
