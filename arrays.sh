d4a0e04b50cb24377940b42c0d4272e0c738f736
MatsuiMiyako <rowlet997@gmail.com>
2026-04-23 19:01:46 -0500

    Added why we use arrays

--- arrays.sh
+++ arrays.sh
@@ -9,6 +9,9 @@
 # These values can be accessed using an index, which starts at 0.
 # Some languages start their array index at 1, but bash starts at 0, so keep that in mind.
 
-
+# Why use arrays?
+# Arrays are useful for storing and managing collections of data.
+# They allow you to group related data together and access it easily using indices (numeric positions).
+# For example, you can use an array to store a list of names, numbers, or whatever you else want.
 
 
