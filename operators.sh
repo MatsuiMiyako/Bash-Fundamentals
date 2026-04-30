357db322e7e6cf9738df26db6a58e5296f99d9a0
MatsuiMiyako <rowlet997@gmail.com>
2026-04-30 10:17:05 -0500

    explained what operators are

--- operators.sh
+++ operators.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Operators
+
+# What are operators?
+# Operators in Bash are used to determine if a condition is true or false.
+# Basically, it allows your scripts to respond differently based on different conditions.
+
