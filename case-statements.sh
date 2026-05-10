469422fcd2ff1613f73e7a658def23294e2da9e5
MatsuiMiyako <rowlet997@gmail.com>
2026-05-10 16:44:03 -0500

    Added example 1

--- case-statements.sh
+++ case-statements.sh
@@ -26,3 +26,27 @@
 #         # code to execute if variable does not match any pattern
 #         ;;
 # esac
+
+# Example 1):
+read -p "Enter a Pokemon: " pokemon
+case $pokemon in
+    "Pikachu")
+        echo "Pikachu, the Mouse Pokémon."
+        ;;
+    "Charmander")
+        echo "Charmander, the Lizard Pokémon."
+        ;;
+    "Squirtle")
+        echo "Squirtle, the Tiny Turtle Pokémon."
+        ;;
+    "Bulbasaur")
+        echo "Bulbasaur, the Seed Pokémon."
+        ;;
+    *)
+        echo "Either yuno ball or that isn't a Pokemon!"
+        ;;
+esac
+# The user's input is stored in the variable `pokemon` and it is compared against the patterns "Pikachu", "Charmander", "Squirtle", and "Bulbasaur".
+# If any of it matches, the corresponding code block is executed. 
+# If none of the patterns match, the code block under `*` is executed, which serves as a default case.
+
