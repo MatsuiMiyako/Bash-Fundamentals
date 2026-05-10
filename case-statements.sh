f1bea280b6cb43ce8c6857ab94a94ddf5d675788
MatsuiMiyako <rowlet997@gmail.com>
2026-05-10 16:32:10 -0500

    Added why we use case statements

--- case-statements.sh
+++ case-statements.sh
@@ -8,3 +8,7 @@
 # Case statements in bash are a way to match one value against a list of patterns and run code based on the first match.
 # They can be compared to be a more powerful version of if-else statements, especially when you have multiple conditions to check.
 
+# Why use Case Statements?
+# Case statements are useful for handling multiple conditions in a cleaner and more readable way than if-else statements.
+# They allow you to match patterns using asterisks and can be more efficient when you have many conditions to check.
+
