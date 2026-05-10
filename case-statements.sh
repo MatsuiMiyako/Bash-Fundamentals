fb14c94d049b759f73632274a4356863339dd665
MatsuiMiyako <rowlet997@gmail.com>
2026-05-10 16:31:12 -0500

    Added case statements description

--- case-statements.sh
+++ case-statements.sh
@@ -5,3 +5,6 @@
 # Bash Case Statements
 
 # What are Case Statements?
+# Case statements in bash are a way to match one value against a list of patterns and run code based on the first match.
+# They can be compared to be a more powerful version of if-else statements, especially when you have multiple conditions to check.
+
