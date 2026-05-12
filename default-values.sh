30a8f198ccc15d25b11188dc81671bf9b2822e2e
MatsuiMiyako <rowlet997@gmail.com>
2026-05-12 17:52:34 -0500

    Added examples of what to avoid

--- default-values.sh
+++ default-values.sh
@@ -35,7 +35,6 @@ echo "Your favorite color is ${color:=blue}."
 
 # What not to do:
 # Don't forget that default values only work when the variable is null or unset
-# Don't use default values if you want to check for specific conditions
 # Don't use default values if you want to provide a default value for a variable that is already set
 
 # Examples of what to avoid:
@@ -45,3 +44,9 @@ echo "Your favorite color is ${color:=blue}."
 # Output: Hello, Alice!
 # Error: You are using a default value when the variable `name` is already set to "Alice". 
 # The default value "stranger" will not be used.
+
+# color="red"
+# echo "Your favorite color is ${color:=blue}."
+# Output: Your favorite color is red.
+# Error: You are using a default value when the variable `color` is already set to "red".
+
