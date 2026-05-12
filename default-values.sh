4d5e07a871bf721042e9a4792c279b3e8e390966
MatsuiMiyako <rowlet997@gmail.com>
2026-05-12 10:12:43 -0500

    added description of default values

--- default-values.sh
+++ default-values.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Default Values
+
+# What are default values?
+# Default values in bash are used to assign a value to a variable if it is not already set or if it is null.
+# We do this because sometimes we want a variable to have a value even if the user doesn't assign one.
+
+# Why use default values?
+# Default values are useful when your script needs certain variables, and they need to have a value 
