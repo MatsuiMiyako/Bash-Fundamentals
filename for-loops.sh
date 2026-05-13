d8390a95fa13b07c98829e53a44a5811e54d679a
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:46:23 -0500

    added reason

--- for-loops.sh
+++ for-loops.sh
@@ -6,3 +6,7 @@
 
 # What is a for loop?
 # For loops allow you to execute a block of code repeatedly for a specified number of times or over a range of values.
+
+# Why use for loops?
+# For loops are useful for iterating over lists, arrays, or ranges of numbers.
+# They allow you to automate repititive tasks without having to write the same code multiple times.
