db4a50e10063c1f8ba1a0e6ada104a909a752b29
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:20:01 -0500

    added reason

--- redirects.sh
+++ redirects.sh
@@ -8,3 +8,6 @@
 # Redirects in bash are used to change the input or output of a command.
 # This allows you to save the output of a command to a file, or to use the output of a command as input to another command.
 
+# Why use redirects?
+# Redirects are useful when you want to save the output of a command to a file, 
+# or when you want to use the output of a command as input to another command without using pipes.
