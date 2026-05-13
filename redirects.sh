e49807b6837c3894d69aa1414a42a0b18a0013ae
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:18:27 -0500

    added description

--- redirects.sh
+++ redirects.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Redirects
+
+# What are redirects?
+# Redirects in bash are used to change the input or output of a command.
+# This allows you to save the output of a command to a file, or to use the output of a command as input to another command.
+
