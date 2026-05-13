d15db72e951d5e1d3e2e936ec55e003bee91b22c
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:23:32 -0500

    Added syntax for redirects

--- redirects.sh
+++ redirects.sh
@@ -11,3 +11,14 @@
 # Why use redirects?
 # Redirects are useful when you want to save the output of a command to a file, 
 # or when you want to use the output of a command as input to another command without using pipes.
+
+# How do redirects work?
+# There are several types of redirects in bash:
+# 1) Output Redirects:
+#    - `>`: Redirects the standard output (stdout) of a command to a file, overwriting the file if it already exists.
+#    - `>>`: Redirects the standard output (stdout) of a command to a file, appending to the file if it already exists.
+# 2) Input Redirects:
+#    - `<`: Redirects the standard input (stdin) of a command from a file
+# 3) Error Redirects:
+#    - `2>`: Redirects the standard error (stderr) of a command to a file, overwriting the file if it already exists.
+#    - `2>>`: Redirects the standard error (stderr) of a command to a file, appending to the file if it already exists.
