62f7f2203c1477e05377901ebd2c2ec7fd0d5cdc
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:01:56 -0500

    Added description

--- pipes.sh
+++ pipes.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Pipes
+
+# What are pipes?
+# Pipes in bash are used to connect the output of one command to the input of another command.
+# This basically allows you to chain commands together.
+
