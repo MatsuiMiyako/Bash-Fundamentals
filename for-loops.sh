33d2186e3c4c25e14c2d12ae8e110d29b815dab9
MatsuiMiyako <rowlet997@gmail.com>
2026-05-13 10:45:32 -0500

    added description

--- for-loops.sh
+++ for-loops.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash For Loops
+
+# What is a for loop?
+# For loops allow you to execute a block of code repeatedly for a specified number of times or over a range of values.
