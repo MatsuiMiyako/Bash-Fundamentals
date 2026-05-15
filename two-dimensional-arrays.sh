2bde45e40e131678f1b825007492faa8aa83fa54
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:20:34 -0500

    added syntax

--- two-dimensional-arrays.sh
+++ two-dimensional-arrays.sh
@@ -10,4 +10,9 @@
 # Why use two-dimensional arrays?
 # They allow you to group related data together in a more organized way than using a single array.
 
+# How do two-dimensional arrays work?
+# To create a two-dimensional array in bash, you can use the following syntax:
+# array_name=( (value1 value2) (value3 value4) ... )
+# You can also create a two-dimensional array using the `declare` command:
+# declare -a array_name
 
