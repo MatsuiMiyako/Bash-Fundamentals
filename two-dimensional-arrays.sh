137474dd7d9a813b34e1f261d81762300a6d52cb
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:20:02 -0500

    added reason

--- two-dimensional-arrays.sh
+++ two-dimensional-arrays.sh
@@ -7,3 +7,7 @@
 # What are two-dimensional arrays?
 # Two-dimensional arrays are an array of arrays. They allow you to store and manage data in a grid.
 
+# Why use two-dimensional arrays?
+# They allow you to group related data together in a more organized way than using a single array.
+
+
