689fd8b50ff141c8a1d45acf3e327b07776430af
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:03:55 -0500

    added reason

--- functions.sh
+++ functions.sh
@@ -8,3 +8,6 @@
 # A function is a reusable block of code. 
 # It allows you to group a set of commands together and execute them whenever needed.
 
+# Why use functions?
+# Functions help you organize your code and avoid repetition.
+# Using functions means you can call a block of code to use later and multiple times without having to rewrite it.
