b9096b61d1b13d649d4804ac8631cbf1d94eca71
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:04:20 -0500

    added functions syntax

--- functions.sh
+++ functions.sh
@@ -11,3 +11,9 @@
 # Why use functions?
 # Functions help you organize your code and avoid repetition.
 # Using functions means you can call a block of code to use later and multiple times without having to rewrite it.
+
+# How do functions work?
+# Here is the basic syntax for a function:
+# function_name() {
+#     # do something exciting here
+# }
