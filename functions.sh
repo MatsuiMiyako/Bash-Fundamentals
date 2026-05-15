c428f232212313adec8ca160ca754a046b5423da
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:02:44 -0500

    added description

--- functions.sh
+++ functions.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash Functions
+
+# What is a function?
+# A function is a reusable block of code. 
+# It allows you to group a set of commands together and execute them whenever needed.
+
