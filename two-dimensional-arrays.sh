b7cc46798442b49bd7f32bd3e264ccf5699fb140
MatsuiMiyako <rowlet997@gmail.com>
2026-05-15 10:19:00 -0500

    added deription

--- two-dimensional-arrays.sh
+++ two-dimensional-arrays.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Bash two-dimensional arrays
+
+# What are two-dimensional arrays?
+# Two-dimensional arrays are an array of arrays. They allow you to store and manage data in a grid.
+
